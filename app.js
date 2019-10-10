13f2bc66eb7af97a55f6186a374b7b937e5ee913
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2019-10-10 23:15:11 +0900

    Base structure and styling

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = () => {
+  const playBtn = document.querySelector('.player-timer-btn');
+
+  playBtn.addEventListener('click', () => {
+    console.log('btn is clicked');
+  })
+}
+
+app();
